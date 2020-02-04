09128e1135ca6fdf28795c703ba1b0ab349d0237
birkastoana <birkastoana@gmail.com>
2020-02-04 15:54:00 +0100

    added config/master.key to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :repo_url, "git@github.com:birkastoana/cookbook.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+ append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
